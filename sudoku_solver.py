bbd320d73d6fe7d02d086ea0b70ee07390c39158
arnavakula <akula.arnav@gmail.com>
2022-07-22 11:54:02 -0700

    Scrape and solve puzzle from nytimes

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,31 @@
-example_board = [[0, 0, 0, 0, 0, 1, 0, 9, 0], [0, 2, 4, 3, 6, 0, 0, 0, 0], [6, 1, 0, 0, 0, 0, 4, 0, 0], [8, 6, 0, 5, 9, 3, 7, 0, 4], [2, 7, 9, 6, 0, 4, 5, 0, 1], [0, 0, 3, 0, 0, 0, 9, 0, 8], [7, 0, 0, 9, 1, 0, 0, 4, 0], [0, 0, 2, 0, 0, 0, 6, 0, 0], [0, 0, 0, 8, 0, 6, 3, 7, 0]]
+import time
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+
+example_board = [[0, 6, 4, 8, 7, 0, 0, 0, 9], [0, 0, 3, 9, 0, 0, 2, 5, 0], [0, 0, 7, 3, 0, 2, 0, 0, 4], [0, 0, 2, 0, 5, 4, 0, 9, 0], [9, 0, 0, 0, 0, 0, 0, 0, 6], [0, 3, 0, 6, 9, 0, 1, 0, 0], [3, 0, 0, 4, 0, 8, 9, 0, 0], [0, 2, 9, 0, 0, 1, 4, 0, 0], [8, 0, 0, 0, 3, 9, 5, 6, 0]]
+def enter_nums(board):
+    for i in range(0, 9):
+        for j in range(0, 9):
+            board[i][j] = int(input("enter:"))
+    
+    print(board)
+
+def scrape_puzzle(mode):
+    url = 'https://www.nytimes.com/puzzles/sudoku/{}'.format(mode)
+    soup = BeautifulSoup(requests.get(url).content,"html.parser")
+
+    data = json.loads(re.sub('window.gameData = ', '', soup.find('script').contents[0]))
+
+    puzzle = data[mode]['puzzle_data']['puzzle']
+
+    board = []
+    for i in range(9):
+        board.append(puzzle[9 * i:9 * i + 9])
+
+    return board
+
 
 def solve(board):
     #try to find empty slot
@@ -48,13 +75,23 @@ def is_valid_move(board, i, j, num):
 
     return True
 
-def main(board):
+def main():
+    t1 = time.time()
+
+    board = scrape_puzzle('hard')
+
     solve(board)
 
     for row in board:
         print(row)
+    
+    t2 = time.time()
+    print("Time to solve: %rs" % (round(t2 - t1, 5)))
         
-main(example_board)
+# main(example_board)
+if __name__ == '__main__':
+    main()
+
 
 
 #PSUEDOCODE
